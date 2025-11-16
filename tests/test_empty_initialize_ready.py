3f8060856f2618e4c047e0ae61b256edc8cdb6df
polllaris <polllaris@protonmail.com>
2025-11-16 18:50:08 -0500

    add test to verify that no guilds will be seen on the client after initializing when not in any

--- tests/test_empty_initialize_ready.py
+++ tests/test_empty_initialize_ready.py
@@ -17,6 +17,18 @@ class TestOnReadyEventAfterSimpleInitialization(unittest.IsolatedAsyncioTestCase
         # discord.py tends to take 2 seconds to trigger on ready, even when there are no guilds.
         await wait_helper.wait_for_trigger(3)
 
+class TestClientStateGuildInfoAfterOnReady(unittest.IsolatedAsyncioTestCase):
+    async def asyncSetUp(self):
+        self.client = discord.Client(intents=discord.Intents.default())
+        self.harness = Harness()
+        self._ready_event_wait_helper = EventWaitHelper.using_client(self.client, "on_ready")
+        await self.harness.users.new_user("Name123")
+        await self.harness.initialize(self.client, "Name123")
+        await self._ready_event_wait_helper.wait_for_trigger(3)
+
+    async def test_should_see_no_guilds_on_client_after_ready_event_from_initializing_when_not_in_any(self):
+        self.assertEqual(0, len(self.client.guilds))
+
 
 class TestClientStateUserInfoAfterOnReady(unittest.IsolatedAsyncioTestCase):
     async def asyncSetUp(self):
