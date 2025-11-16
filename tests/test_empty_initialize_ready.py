385f15f30b1d34dc0b5af66ac50d972192845c36
polllaris <polllaris@protonmail.com>
2025-11-16 18:08:23 -0500

    add tests to verify that the user data on the client is as expected after the on_ready event from initializing with the harness
    It should have the username specified, as it is immediately relevant. And it should have the userid we hardcoded for now. This will fail as the implementation has not been updated yet.

--- tests/test_empty_initialize_ready.py
+++ tests/test_empty_initialize_ready.py
@@ -1,5 +1,8 @@
 import discord
 import unittest
+
+from aiohttp.log import client_logger
+
 from discord_harness import Harness
 from tests import EventWaitHelper
 
@@ -16,3 +19,23 @@ class TestOnReadyEventAfterSimpleInitialization(unittest.IsolatedAsyncioTestCase
         await harness.initialize(client, "November")
         # discord.py tends to take 2 seconds to trigger on ready, even when there are no guilds.
         await wait_helper.wait_for_trigger(3)
+
+
+class TestClientStateUserInfoAfterOnReady(unittest.IsolatedAsyncioTestCase):
+    async def asyncSetUp(self):
+        self.client = discord.Client(intents=discord.Intents.default())
+        self.harness = Harness()
+        self._ready_event_wait_helper = EventWaitHelper.using_client(self.client, "on_ready")
+
+    async def test_should_see_right_username_on_client_after_ready_event_from_initializing(self):
+        await self.harness.users.new_user("DecemberSnow")
+        await self.harness.initialize(self.client, "DecemberSnow")
+        await self._ready_event_wait_helper.wait_for_trigger(3)
+        self.assertEqual("DecemberSnow", self.client.user.name)
+
+    async def test_should_see_right_discord_id_on_client_after_ready_event_from_initializing(self):
+        # We're not doing anything with it yet, so we'll just expect 555222333 for now.
+        await self.harness.users.new_user("OctoberLeaves")
+        await self.harness.initialize(self.client, "OctoberLeaves")
+        await self._ready_event_wait_helper.wait_for_trigger(3)
+        self.assertEqual(555222333, self.client.user.id)
