bc7093be17daaa08d001e92915ce85188d83e340
polllaris <polllaris@protonmail.com>
2025-11-16 17:24:34 -0500

    add start of "gateway ready" payload data making code with some simple tests
    The discord API will send a ready event with a list of each guild the user is in, with the ID and a field stating it's unavailable. Each guild the user is in will have a GUILD_CREATE event sent later with the information for it.
    
    Some basic data has been filled in to start, with some default values for some stuff not specified. This will be driven further by what is necessary to test the harness/bot integration.
    
    The avatar field is mandatory for discord.py .

---
+++ src/discord_harness/payloads/__init__.py
@@ -0,0 +1,18 @@
+
+
+def make_ready_payload(guild_ids: list[int]) -> dict:
+    """
+    Makes the payload the client should receive after connecting to the gateway,
+    with a list of each 'unavailable guild' id the bot is in, and default values for everything else.
+    """
+    user_data = {
+        "id": 555222333,
+        "username": "gatedway",
+        "discriminator": "5001",
+        "avatar": "6209f9e346b1ba4f4ca6d373bafb860b",
+    }
+    unavailable_guilds = []
+    for guild_id in guild_ids:
+        # "guilds are the guilds of which your bot is a member. They start out as unavailable when you connect to the gateway. As they become available, your bot will be notified via Guild Create events."
+        unavailable_guilds.append({"id": guild_id, "unavailable": True})
+    return {"guilds": unavailable_guilds, "user": user_data}
---
+++ tests/payloads/test_ready_payload_making.py
@@ -0,0 +1,60 @@
+import unittest
+from discord_harness.payloads import make_ready_payload
+
+
+def _make_payload(guilds: list[int] | None = None) -> dict:
+    guilds = guilds if guilds is not None else []
+    return make_ready_payload(guilds)
+
+
+class TestGatewayReadyPayloadCreationGuildData(unittest.TestCase):
+    """
+    The ready event data should contain a list of data for "unavailable guilds",
+    that is a list of ids of guilds to be streamed in to the client, and a {unavailable: true}.
+    """
+    # "guilds are the guilds of which your bot is a member. They start out as unavailable when you connect to the gateway. As they become available, your bot will be notified via Guild Create events."
+    def test_should_have_empty_list_for_guilds_when_no_guild_ids_provided(self):
+        payload = _make_payload(guilds=[])
+        self.assertEqual([], payload["guilds"])
+        self.assertIsInstance(payload["guilds"], list)
+
+    def test_should_have_guilds_in_list_containing_their_ids_when_ids_provided(self):
+        # https://discord.com:2087/developers/docs/events/gateway-events#ready
+        # A list of unavailable guild objects, they'll have the id under the "id" key
+        payload = _make_payload(guilds=[9, 15])
+        self.assertEqual(9, payload["guilds"][0]["id"])
+        self.assertEqual(15, payload["guilds"][1]["id"])
+
+    def test_should_have_guilds_in_list_containing_unavailable_set_to_true_when_ids_provided(self):
+        # https://discord.com:2087/developers/docs/events/gateway-events#ready
+        # A list of unavailable guild objects, they'll have the true under the "unavailable" key
+        # Each guild the client will see come in through GUILD_CREATE events is placed here.
+        payload = _make_payload(guilds=[25, 30])
+        self.assertTrue(payload["guilds"][0]["unavailable"])
+        self.assertTrue(payload["guilds"][1]["unavailable"])
+
+
+class TestGatewayReadyPayloadCreationUserData(unittest.TestCase):
+
+    def test_should_fill_in_id_field_with_fixed_value_of_555222333(self):
+        # We aren't doing anything with this yet, so we're giving it a fixed number to fill in.
+        user_data = _make_payload()["user"]
+        # Not even bothering to try to give this a valid snowflake ID to start.
+        self.assertEqual(555222333, user_data["id"])
+
+    def test_should_fill_in_username_field_with_fixed_value_of_gatedway(self):
+        # "the user's username, not unique across the platform", bots still use discriminators.
+        user_data = _make_payload()["user"]
+        self.assertEqual("gatedway", user_data["username"])
+
+    def test_should_fill_in_discriminator_field_with_string_value_of_5001(self):
+        # bots still use discriminators
+        user_data = _make_payload()["user"]
+        self.assertEqual("5001", user_data["discriminator"])
+
+    def test_should_fill_in_avatar_with_arbitrary_16_byte_hexadecimal_value(self):
+        # >>> secrets.token_hex(16)
+        # '6209f9e346b1ba4f4ca6d373bafb860b'
+        # It shouldn't matter what value this is, but it MUST be present for discord.py to work with it.
+        user_data = _make_payload()["user"]
+        self.assertEqual("6209f9e346b1ba4f4ca6d373bafb860b", user_data["avatar"])
