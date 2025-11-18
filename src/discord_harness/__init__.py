ace9b7824d4873ec8376628c5e008e26d25daf00
polllaris <polllaris@protonmail.com>
2025-11-18 16:12:48 -0500

    change HarnessUsers.new_user to actually add a new user to system state

--- src/discord_harness/__init__.py
+++ src/discord_harness/__init__.py
@@ -1,16 +1,31 @@
 import discord
+from discord_harness.backend import User
+from discord_harness.backend import SystemState
 from discord_harness.payloads import make_ready_payload
 
 
 class HarnessUsers:
+    def __init__(self, state: SystemState):
+        self._state = state
 
     async def new_user(self, name: str) -> None:
-        pass
+        new_user = User(id_=self._state.next_id(), username=name)
+        self._state.users.add_new_user(new_user)
+
+
+class HarnessGuilds:
+    def __init__(self, state: SystemState):
+        self._state = state
+
+    # async def new_guild(self, guild_name: str, owner_name: str):
+    #     new_guild_id = self._state.next_id()
+    #     new_guild_name =
 
 
 class Harness:
     def __init__(self):
-        self._users = HarnessUsers()
+        system_state = SystemState()
+        self._users = HarnessUsers(system_state)
 
     @property
     def users(self):
---
+++ tests/harness/test_harness_users.py
@@ -0,0 +1,22 @@
+import unittest
+from discord_harness import HarnessUsers
+from discord_harness.backend import SystemState
+
+
+class TestHarnessNewUserCreation(unittest.IsolatedAsyncioTestCase):
+    async def asyncSetUp(self):
+        self._system_state = SystemState()
+        self._harness_users = HarnessUsers(self._system_state)
+
+    async def test_should_make_new_user_and_add_to_system_state_with_name_given(self):
+        await self._harness_users.new_user("NamedPerson")
+        self.assertIsNotNone(self._system_state.users.find_by_username("NamedPerson"))
+
+    # TODO This is getting scrapped later for snowflake ids, but for exploration this is the right design.
+    async def test_should_make_new_users_and_add_to_system_state_with_incrementing_ids(self):
+        await self._harness_users.new_user("Name123")
+        await self._harness_users.new_user("Name456")
+        added_user_one = self._system_state.users.find_by_username("Name123")
+        added_user_two = self._system_state.users.find_by_username("Name456")
+        self.assertEqual(1, added_user_one.id)
+        self.assertEqual(2, added_user_two.id)
