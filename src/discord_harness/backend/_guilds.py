1a20bce4c52da621ed9a1985e385f87e9d3dd2ac
polllaris <polllaris@protonmail.com>
2025-11-18 20:18:26 -0500

    move Guilds and Guild from backend.__init__.py to backend._guilds.py and keep available in __init__.py

---
+++ src/discord_harness/backend/_guilds.py
@@ -0,0 +1,39 @@
+
+
+class Guild:
+    def __init__(self, id_: int, name: str, owner_id: int):
+        self.id = id_
+        self.name = name
+        self.owner_id = owner_id
+        self._members = list()
+
+    @property
+    def members(self) -> list[int]:
+        """
+        The list of identifiers for members who are in the guild.
+        """
+        return self._members
+
+
+class Guilds:
+    def __init__(self):
+        self._guild_list: list[Guild] = []
+
+    def add_new_guild(self, guild: Guild) -> None:
+        self._guild_list.append(guild)
+
+    def get_all_guilds(self) -> list[Guild]:
+        return self._guild_list.copy()
+
+    def get_guilds_by_member_id(self, member_id: int) -> list[Guild]:
+        guilds_with_member = []
+        for guild in self._guild_list:
+            if member_id in guild.members:
+                guilds_with_member.append(guild)
+        return guilds_with_member
+
+    def find_guild_by_name(self, guild_name: str) -> Guild | None:
+        for guild in self._guild_list:
+            if guild.name == guild_name:
+                return guild
+        return None
