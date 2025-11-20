b03c6d2f72fcf9416ada7d7e63d16c55234a675b
polllaris <polllaris@protonmail.com>
2025-11-19 23:42:26 -0500

    add create_user_and_guild method to HarnessPiecesTestFixture to streamline creating an arbitrary user and making a guild as them in this section

--- tests/harness/__init__.py
+++ tests/harness/__init__.py
@@ -9,3 +9,7 @@ class HarnessPiecesTestFixture(unittest.IsolatedAsyncioTestCase):
         self._system_state = SystemState()
         self._harness_users = HarnessUsers(self._system_state)
         self._harness_guilds = HarnessGuilds(self._system_state)
+
+    async def create_user_and_guild(self, new_user_name: str, new_guild_name: str):
+        await self._harness_users.new_user(new_user_name)
+        await self._harness_guilds.new_guild(new_guild_name, new_user_name)
