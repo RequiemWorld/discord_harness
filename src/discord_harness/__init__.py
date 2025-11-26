81a63921673e2b2b35ad442c96f91cbe973fcead
polllaris <polllaris@protonmail.com>
2025-11-25 22:56:46 -0500

    refactor Harness to use GatewayInformationInterface / SystemStateGatewayInformationInterface for gateway ready info

--- src/discord_harness/__init__.py
+++ src/discord_harness/__init__.py
@@ -6,6 +6,8 @@ from discord_harness.backend import SystemState
 from discord_harness.backend.interface import UserCreationRequest
 from discord_harness.backend.interface import DiscordBackendUsers
 from discord_harness.backend.interface import SystemStateBackendUsers
+from discord_harness.backend.interface import GatewayInformationInterface
+from discord_harness.backend.interface import SystemStateGatewayInformationInterface
 from discord_harness.payloads import make_ready_payload
 
 
@@ -80,6 +82,7 @@ class Harness:
     def __init__(self):
         self._state = SystemState()
         backend_users = SystemStateBackendUsers(self._state)
+        self._backend_gateway = SystemStateGatewayInformationInterface(self._state)
         self._users = HarnessUsers(backend_users)
         self._guilds = HarnessGuilds(self._state)
 
@@ -103,8 +106,8 @@ class Harness:
         # ConnectionState.parse_x is where the data off the websocket would usually get handled,
         # we can create the data as necessary and inject it there after we've called the setup hook.
         discord_id = self._state.users.find_id_for_username(discord_name)
-        guild_ids_user_in = [guild.id for guild in self._state.guilds.get_guilds_by_member_id(discord_id)]
-        ready_event_data = make_ready_payload(username=discord_name, guild_ids=guild_ids_user_in)
+        ready_info = await self._backend_gateway.get_ready_info(discord_id)
+        ready_event_data = make_ready_payload(username=discord_name, guild_ids=ready_info.unavailable_guild_ids)
         connection_state.parse_ready(ready_event_data)
         for guild in self._state.guilds.get_guilds_by_member_id(discord_id):
             # discord.Client.guilds will get populated with only id and name in the data handled
