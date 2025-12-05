7f465ebc50b03fc127bfce0e4b2ffdfaecd94087
polllaris <polllaris@protonmail.com>
2025-12-05 05:36:07 -0500

    add pull_ready_info_for_user method to GatewayInformationPuller with implementation on SystemStateGatewayInformationPuller and existing but raising NotImplementedError on MockGatewayInformationPuller

--- src/discord_harness/database/_utilities/_gateway_info_puller.py
+++ src/discord_harness/database/_utilities/_gateway_info_puller.py
@@ -1,9 +1,17 @@
 import abc
 from ..information import GuildCreateInfo
+from ..information import ReadyInfo
 from .._entities import SystemState
 
 
 class GatewayInformationPuller(abc.ABC):
+
+    @abc.abstractmethod
+    def pull_ready_info_for_user(self, user_id: int) -> ReadyInfo:
+        """
+        :raises ValueError: When no user with the given id exists to pull ready information for.
+        """
+        raise NotImplementedError
 
     @abc.abstractmethod
     def pull_guild_create_info_for_guild(self, guild_id: int) -> GuildCreateInfo | None:
@@ -11,20 +19,31 @@ class GatewayInformationPuller(abc.ABC):
 
 
 class MockGatewayInformationPuller(GatewayInformationPuller):
+
     def __init__(self):
         self._guild_ids_to_preset_info = {}
 
     def set_result_for_guild_id(self, guild_id: int, result: GuildCreateInfo):
         self._guild_ids_to_preset_info[guild_id] = result
 
+    def pull_ready_info_for_user(self, user_id: int) -> ReadyInfo:
+        raise NotImplementedError
+
     def pull_guild_create_info_for_guild(self, guild_id: int) -> GuildCreateInfo | None:
         return self._guild_ids_to_preset_info.get(guild_id)
 
 
-
 class SystemStateGatewayInformationPuller(GatewayInformationPuller):
+
     def __init__(self, system_state: SystemState):
         self._state = system_state
+
+    def pull_ready_info_for_user(self, user_id: int) -> ReadyInfo:
+        found_user = self._state.users.find_by_id(user_id)
+        if found_user is None:
+            raise ValueError(f"no user with the id {user_id} could be found to pull ready info for.")
+        ids_of_guilds_user_is_in = [guild.id for guild in self._state.guilds.get_guilds_by_member_id(user_id)]
+        return ReadyInfo(found_user.id, found_user.username, unavailable_guild_ids=ids_of_guilds_user_is_in)
 
     def pull_guild_create_info_for_guild(self, guild_id: int) -> GuildCreateInfo | None:
         found_guild = self._state.guilds.find_guild_by_id(guild_id)
--- tests/database/test_state_gateway_info_puller.py
+++ tests/database/test_state_gateway_info_puller.py
@@ -1,4 +1,5 @@
 import unittest
+from discord_harness.database import User
 from discord_harness.database import Guild
 from discord_harness.database import SystemState
 from discord_harness.database import SystemStateGatewayInformationPuller
@@ -26,3 +27,36 @@ class TestGuildCreatePullInfoCorrectness(unittest.TestCase):
 
     def test_should_return_result_with_right_guild_name(self):
         self.assertEqual("SomeGuild", self._pull_result.guild_name)
+
+
+# FIXME These are somewhat difficult to write and not well factored. Codebase needs restructuring and fixing ASAP.
+class TestReadyInfoPullMethod(unittest.TestCase):
+    def setUp(self):
+        self._state = SystemState()
+        self._puller = SystemStateGatewayInformationPuller(self._state)
+        always_existing_user = User(id_=444888, username="SomeName")
+        self._state.users.add_new_user(always_existing_user)
+
+    def test_should_raise_value_error_when_user_with_id_does_not_exist(self):
+        with self.assertRaises(ValueError):
+            self._puller.pull_ready_info_for_user(user_id=222333)
+
+    def test_should_return_result_with_empty_unavailable_guild_ids_list_when_user_not_in_any(self):
+        ready_info = self._puller.pull_ready_info_for_user(444888)
+        self.assertEqual([], ready_info.unavailable_guild_ids)
+
+    def test_should_return_result_with_unavailable_guild_ids_containing_every_guild_user_is_in(self):
+        guild_one = Guild(id_=1234, name="Guild1", owner_id=999)
+        guild_two = Guild(id_=5678, name="Guild2", owner_id=999)
+        guild_one.members.append(444888)
+        guild_two.members.append(444888)
+        self._state.guilds.add_new_guild(guild_one)
+        self._state.guilds.add_new_guild(guild_two)
+        self.assertEqual({1234, 5678}, set(self._puller.pull_ready_info_for_user(444888).unavailable_guild_ids))
+
+    def test_should_return_result_with_right_userid(self):
+        self.assertEqual(444888, self._puller.pull_ready_info_for_user(444888).userid)
+
+    def test_should_return_result_with_right_username(self):
+        self.assertEqual("SomeName", self._puller.pull_ready_info_for_user(444888).username)
+
